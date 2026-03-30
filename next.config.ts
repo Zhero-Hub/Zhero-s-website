d6e26f619c76cbd14a391b8021dc0c8aee305bf8
Honghao_ZHAO <honghaozhao.bio@gmail.com>
2026-03-30 23:08:45 +0800

    v1.2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/Zhero-s-website',
   trailingSlash: true,
   images: {
     unoptimized: true,
